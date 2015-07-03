f214fa4603f691c14b1edd629b63bd370b333c54
Ken <khale426@gmail.com>
2015-07-03 19:45:43 -0400

    address_bloc

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,18 @@
-puts "Welcome to AddressBloc!"
+
+def greetings
+    # Starts a new line after running 'greeting.rb'.
+    puts ''
+
+    ARGV.each do |arg|
+        # Avoid print '<greeting>, <greeting>' to the screen.
+        unless arg == ARGV[0]
+            # DRY: Reuse the first value in the arguments array.
+            # This makes the code more reusable, and it allows
+            # one to inject any greeting of choice as the first
+            # argument in the 'ARGV' array.
+            puts "#{ARGV[0]}, #{arg}"
+        end
+    end
+end
+
+greetings
